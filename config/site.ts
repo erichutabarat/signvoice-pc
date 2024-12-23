b6c27b3bc0b52fad3439caf9c48e32d64c784e31
Eric Hutabarat <handsomeware.sec@gmail.com>
2024-12-23 15:34:38 +0700

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,9 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "SignVoice",
+  description:
+    "Bridging Silence with Speech, Empowering Communication for Everyone.",
   navItems: [
     {
       label: "Home",
