dd04db18f9cbe45afe8294270817fa155d05d0d6
Eric Hutabarat <handsomeware.sec@gmail.com>
2024-12-23 15:23:06 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    experimental: {
+      esmExternals: true, // Ensures proper handling of ESM dependencies
+    },
+  };
+  
+  module.exports = nextConfig;
+  
